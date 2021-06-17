95fc52ac7ba387fac6253e6dd9ca18200b020a8e
Faitholo <ikhidero.faithy@gmail.com>
2021-06-17 15:30:37 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-init main(void)
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
