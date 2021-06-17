49e532851d3cc569eb1de614325a73bf3adb08cc
Faitholo <ikhidero.faithy@gmail.com>
2021-06-17 16:31:01 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-  printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
