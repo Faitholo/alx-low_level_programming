7f423c60d718ad521b0ec2c7f5b40cb89aca08fd
Faith Ikhidero <ikhidero.faithy@gmail.com>
2021-06-17 07:40:49 -0700

    Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
