2e64bfcd0bd62292f66b1007b37daccd5538f846
Faitholo <ikhidero.faithy@gmail.com>
2021-06-18 17:02:17 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Lanzador.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
