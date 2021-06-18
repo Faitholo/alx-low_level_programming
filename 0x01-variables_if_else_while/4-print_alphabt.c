c2a96f8f70ebe733e048bff944f820c22e5c9758
Faitholo <ikhidero.faithy@gmail.com>
2021-06-18 16:59:39 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Lanzador.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if ((a != 'e') && (a != 'q'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
