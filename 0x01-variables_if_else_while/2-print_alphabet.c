a00a5bf18a051120f2cbd5bb2bdf1423d707c5ff
Faitholo <ikhidero.faithy@gmail.com>
2021-06-18 16:55:04 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Lanzador.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
