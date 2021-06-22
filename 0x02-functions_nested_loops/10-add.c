fa048e9848aba5d7d202f4478152e079888282c5
Faitholo <ikhidero.faithy@gmail.com>
2021-06-22 16:45:57 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - Suma dos enteros por función.
+ * @i: Primera varable entera a sumar.
+ * @l: Segunda variable a sumar.
+ * Return: retorno con sumatoria
+ */
+int add(int i, int l)
+{
+	int n;
+	 n = i + l;
+	return (n);
+}
