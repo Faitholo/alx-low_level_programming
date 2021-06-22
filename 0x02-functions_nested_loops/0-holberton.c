a0bd35b987f5f6b07a7df23ce6c94a969ea22e83
Faitholo <ikhidero.faithy@gmail.com>
2021-06-22 16:10:26 +0100

    Create 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* main - Lanzador
+*
+* Return: Always 0
+*
+*/
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
