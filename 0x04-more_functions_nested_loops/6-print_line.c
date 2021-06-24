d41508d34ced16747e97dd88c728ea7048db7f4c
Faitholo <ikhidero.faithy@gmail.com>
2021-06-24 13:02:03 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * print_line - function that prints a straight line
+  * @n: takes in an integer
+  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
