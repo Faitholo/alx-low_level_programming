293d08a5c098054e1e290bfd5e73d76e01ac0904
Faitholo <ikhidero.faithy@gmail.com>
2021-06-24 12:46:44 +0100

    Create holberton.h

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+void print_number(int n);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
