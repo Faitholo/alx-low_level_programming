fa646ae3ae0c9147a42ac59e82a1a452046566f8
Faitholo <ikhidero.faithy@gmail.com>
2021-06-24 12:51:58 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - checks for lowercase character.
+ * @a: entero recibo por main.
+ * @b: entero recibido por main
+ * Return: 1 if c is uppercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
