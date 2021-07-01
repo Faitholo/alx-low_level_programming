7da5cb78f9c36f545e20d79c3c924b153195c608
Faitholo <ikhidero.faithy@gmail.com>
2021-07-01 03:45:49 +0100

    Update 102-infinite_add.c

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,47 +1,53 @@
 #include "holberton.h"
-
 /**
- * infinite_add - adds two numbers
+ * infinite_add -  adds two numbers
  * @n1: first number
  * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
  *
- * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
+{
+/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
-		n = m;
+		i--;
+		j--;
 		if (i >= 0)
-			n += n1[i] - '0';
+			f = n1[i] - '0';
+		else
+			f = 0;
 		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	if (d == 1)
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
 	return (r);
 }
