5b227d529e3c56669c215dfce98637a8f4f9089a
Faitholo <ikhidero.faithy@gmail.com>
2021-07-01 03:35:57 +0100

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
-/**
- * rot13 - check the code for Holberton School students.
- *@n: the variable
- *Return: n variable
- */
-char *rot13(char *n)
-{
-	int c, j;
-	char alp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (c = 0; n[c] != '\0'; c++)
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; alp[j] != '\0'; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (n[c] == alp[j])
+			if (s[i] == a[j])
 			{
-				n[c] = rot[j];
+				s[i] = b[j];
 				break;
 			}
 		}
 	}
-
-	return (n);
+	return (s);
 }
+
