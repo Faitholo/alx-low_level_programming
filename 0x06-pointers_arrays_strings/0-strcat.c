c7a34d3144ecd27c286a4a606053ed2db2be3cf3
Faitholo <ikhidero.faithy@gmail.com>
2021-07-01 03:32:21 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,28 @@
 #include "holberton.h"
-
 /**
-*_strcat - Funtion that concatenate 2 string
-*
-*@dest: string 1 
-*@src: string 2 
-*Return: Both Strings
-*/
-
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i;
+	int j;
 
-	for (; dest[i]; i++)
-	;
-	while (*src)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[i] = src[j];
-		j++;
 		i++;
 	}
-	i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (dest);
-
 }
