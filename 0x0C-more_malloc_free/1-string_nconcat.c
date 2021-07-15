8ac9f8eb15b3dcae3dd383f9d3573c7ad267b0cf
Faitholo <ikhidero.faithy@gmail.com>
2021-07-15 21:19:11 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * *string_nconcat - concatenates two strings
+ * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: limit of s2
  * Return: pointer to new space in memory or null
- **/
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *strDup;
