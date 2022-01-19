1ff70ac637fad2e5873764d113279d2551b200e3
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-01-19 20:06:38 +0100

    linear and binary

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+/**
+* A function that searches for a value in an
+* array of integers using the Linear search algorithm
+* array = is a pointer to the first element of the array to search in
+* size = is the number of elements in array
+* value = is the value to search for
+* Return: first index OR -1
+**/
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+	int flag = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			flag = 1;
+			return (i);
+		}
+	}
+	if (flag != 1)
+		return (-1);
+	return (i);
+}
